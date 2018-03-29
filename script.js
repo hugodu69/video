e15ea6917a81d74909ab0de5a5e2d4623110ffd0
hugodu69 <hugogogo@protonmail.com>
2018-03-29 23:06:35 +0200

    la barre apparait et play paus e marche sur le bouton et sur l'ecran

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var bunny = document.getElementById("bunny");
 
+/*quand on clique sur le bouton play ou sur la video ça fait play pause*/
 document.getElementById("playpause").addEventListener("click", playpause);
+document.getElementById("bunny").addEventListener("click", playpause);
 function playpause () {
 	if (bunny.paused) {bunny.play();}
 	else {bunny.pause();}
 }
+
+
